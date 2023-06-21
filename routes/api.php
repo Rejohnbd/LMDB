78599a8887ad10f7f3f7ee444ff2c4fda1f7071b
rejohnbd <rejohn.cse@gmail.com>
2023-06-22 00:28:10 +0600

    Add Second Mysql Database Connection & API

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\FirstDatabaseController;
+use App\Http\Controllers\SecondDatabaseController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::apiResource('first-db', FirstDatabaseController::class);
+Route::apiResource('second-db', SecondDatabaseController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
